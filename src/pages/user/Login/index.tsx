289d1eb69329858d53ac9e2d6cc3198594d57a4d
SongXueZhi <sxzdhu@gmail.com>
2022-03-23 21:00:05 +0800

    fix: logo not find

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -75,7 +75,7 @@ const Login: React.FC = () => {
         <div className={styles.top}>
           <div className={styles.header}>
             <Link to="/">
-              <img alt="logo" className={styles.logo} src="/reg_logo.svg" />
+              <img alt="logo" className={styles.logo} src="./reg_logo.svg" />
               <span className={styles.title}>RegMiner Data Annotations</span>
             </Link>
           </div>
